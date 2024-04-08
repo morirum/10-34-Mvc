c1e7ceead7748741278445a139217bb6345bcba5
rumeysa yakut <131006594+morirum@users.noreply.github.com>
2024-04-08 18:02:48 +0300

    Add files via upload

---
+++ KategoriController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _10_30.Models.Entity;
+
+namespace _10_30.Controllers
+{
+    public class KategoriController : Controller
+    {
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORILER1.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult YeniKategori(TBLKATEGORİLER1 p1)
+        {
+            db.TBLKATEGORILER1.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult SIL(int id)
+        {
+            var kategori = db.TBLKATEGORILER1.Find(id);
+            db.TBLKATEGORILER1.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktgr = db.TBLKATEGORILER1.Find(id);
+            return View("KategiGetir",ktgr);
+        }
+        public ActionResult Guncelle (TBLKATEGORILER1 p1)
+        {
+            var ktg = db.TBLKATEGORILER1.Find(p1.KATEGORIID);
+            ktg.KATEGORIAD = p1.KATEGORIAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ UrunController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _10_30.Models.Entity;
+namespace MvcDbStok.Controllers
+{
+    namespace _10_30.Controllers
+    {
+        public class UrunController : Controller
+        {
+            // GET: Urun
+            MvcDbStokEntities db = new MvcDbStokEntities();
+
+            public ActionResult Index()
+            {
+                var degerler = db.TBLURUNLER.ToList();
+                return View(degerler);
+            }
+            [HttpGet]
+            public ActionResult UrunEkle()
+            {
+                List<SelectListItem> degerler = (from i in db.TBLKATEGORILER1.ToList()
+                                                 select new SelectListItem
+                                                 {
+                                                     Text = i.KATEGORIAD,
+                                                     Value = i.KATEGORIID.ToString()
+
+                                                 }).ToList();                                                                                              )
+                ViewBag.dgr = degerler;
+                return View();
+            }
+            [HttpPost]
+            public ActionResult UrunEkle(TBLURUNLER p1)
+            {
+                var ktg = db.TBLKATEGORILER1.Where(m => m.KATEGORIID == p1.TBLKATEGORIILER.KATEGORILERID).FirstOrDefault;
+                p1.TBLKATEGORILER = ktg;
+                db.TBLURUNLER.Add(p1);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            public ActionResult SIL(int id)
+            {
+                var urun = db.TBLKATEGORILER1.Find(id);
+                db.TBLKATEGORILER1.Remove(urun);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+        }
+    }
+
+
+
+
+}
+
